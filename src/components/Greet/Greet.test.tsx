750c035965159d6bc01bc5e6b53321187a36eecb
hevedrios <exxed.ridel@gmail.com>
2022-11-04 12:51:57 -0600

    test.only(), test.skip() and describe()

--- src/components/Greet/Greet.test.tsx
+++ src/components/Greet/Greet.test.tsx
@@ -2,14 +2,22 @@ import { render, screen } from '@testing-library/react'
 import Greet from './Greet'
 // Greet needs { } when not default exported
 
-test('Greet renders correctly', () => {
-   render(<Greet />)
-   const textElement = screen.getByText(/hello/i)
-   expect(textElement).toBeInTheDocument()
+describe('Greet', () => {
+   test('Renders correctly', () => {
+      render(<Greet />)
+      const textElement = screen.getByText(/hello/i)
+      expect(textElement).toBeInTheDocument()
+   })
+
+   describe('Nested', () => {
+      test('Renders a name', () => { // test.only(), test.skip()
+         render(<Greet name='Heved' />)
+         const textElement = screen.getByText('Hello Heved')
+         expect(textElement).toBeInTheDocument()
+      })
+   })
 })
 
-test('Greet renders with a name', () => {
-   render(<Greet name='Heved'/>)
-   const textElement = screen.getByText('Hello Heved')
-   expect(textElement).toBeInTheDocument()
-})
+// Several describes is also posible but test suites is still 1 per file
+
+
